cc7be7bc1a19adbb9242192dee850e3665d019af
gjohnson567 <gjohnson@uni.edu.pe>
2024-01-29 17:58:16 -0500

    mi primer proyecto

---
+++ src/main/java/sunat/gob/pe/ejercicio02g7/App.java
@@ -0,0 +1,39 @@
+package sunat.gob.pe.ejercicio02g7;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+    	
+    	Button btnActual = new Button("Actual");
+    	btnActual.setPrefSize(80, 25);
+    	
+    	Button btnProyectado = new Button("Proyectado");
+    	btnProyectado.setPrefSize(80, 25);
+    	
+    	
+        var javaVersion = SystemInfo.javaVersion();
+        var javafxVersion = SystemInfo.javafxVersion();
+
+        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        var scene = new Scene(new StackPane(label), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
